b681ec26dafe9be80cb61785258b79f82c6fbb91
yangtori0407 <heyang236@gmail.com>
2025-02-11 09:34:39 +0900

    countries

---
+++ src/main/java/com/yang/app/countries/CountryDTO.java
@@ -0,0 +1,28 @@
+package com.yang.app.countries;
+
+public class CountryDTO {
+	private String country_id;
+	private String country_name;
+	private long region_id;
+	
+	public String getCountry_id() {
+		return country_id;
+	}
+	public void setCountry_id(String country_id) {
+		this.country_id = country_id;
+	}
+	public String getCountry_name() {
+		return country_name;
+	}
+	public void setCountry_name(String country_name) {
+		this.country_name = country_name;
+	}
+	public long getRegion_id() {
+		return region_id;
+	}
+	public void setRegion_id(long region_id) {
+		this.region_id = region_id;
+	}
+	
+	
+}
---
+++ src/main/java/com/yang/app/countries/CountryService.java
@@ -0,0 +1,16 @@
+package com.yang.app.countries;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.yang.app.ActionForward;
+
+public class CountryService {
+	
+	private CountryDAO countryDAO;
+
+	public void getList(HttpServletRequest request, ActionForward actionForward) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
